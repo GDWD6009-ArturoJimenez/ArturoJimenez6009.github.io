96880f80efe04f33a7507036354e612e32900015
GDWD6009-ArturoJimenez <Arturo.Jimenez@students.first.edu>
2022-12-05 13:34:00 -0500

    Calculator.js

---
+++ Calculator/js/calculator.js
@@ -0,0 +1,16 @@
+const result = document.getElementById ("answer");
+
+function resultAssign () {
+  num = 3 + 5 + 500 + 50 + 87 + 2;
+  result.innerHTML = num;
+  console.log ("Variables num represents the number:" + num);
+}
+
+resultAssign()
+
+
+function addTwoPlusTwo () {
+  result.innerHTML = 2 + 2;
+}
+
+addTwoPlusTwo()
