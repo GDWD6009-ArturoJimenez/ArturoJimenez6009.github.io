24edd162ecd9aa938b07dbfc0afea4505b07ba67
GDWD6009-ArturoJimenez <Arturo.Jimenez@students.first.edu>
2022-12-12 14:28:53 -0500

    StartingGear

--- GSAP1/js/gsap.js
+++ GSAP1/js/gsap.js
@@ -1,5 +1,25 @@
 gsap.from(".chucked", {
-  duration: 5,
+  duration: 6,
   rotation: 720,
-  ease: "bounce"
+  ease: "bounce",
+  x: -500,
+  y: -500,
 });
+
+
+gsap.from(".chucked1", {
+  duration: 7,
+  rotation: 840,
+  ease: "bounce",
+  x: -500,
+  y: -500,
+});
+
+
+gsap.from(".headline", {
+  duration: 2.5,
+  stagger:0.3,
+  opacity:0,
+  y:300,
+})
+
